c28200424b5f4a30a5464083a97651ee7ce4f983
ashley ramirez <arami28@umd.edu>
2026-09-11 11:07:31 -0400

    Added new route for hello HTML page

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World from Ashley Ramirez! I am adding my first code change '
+    return 'Hello World from Ashley Ramirez! I am adding my first code change.'
+
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
 
 if __name__ == '__main__':
     app.run(port=5001)
