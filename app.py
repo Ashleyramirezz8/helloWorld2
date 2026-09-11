3751dc9bb3762906ec82b807026126d7e737789f
ashley ramirez <arami28@umd.edu>
2026-09-11 11:13:38 -0400

    Added code to demonstrate first code change.

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World from Ashley Ramirez! I am adding my first code change.'
+    return 'Hello World from Ashley Ramirez! I am adding my first code change'
 
 @app.route('/hello')
 def hello():
