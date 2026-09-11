16779962096146cd753e75d58ac8a55d65949a0b
ashley ramirez <arami28@umd.edu>
2026-09-10 21:18:39 -0400

    Added first HTML page with Hello World message. Had to change the port to 5001  to avoid conflict.

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    return 'Hello World from Ashley Ramirez!'
+
+@app.route('/hello')
+def hello_page():
+    return 'Hello World from Ashley Ramirez! This is my first HTML page'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=5001)
