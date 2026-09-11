66c73040db0fb84f07ec29b11a8a19405b19d42d
ashley ramirez <arami28@umd.edu>
2026-09-11 10:51:46 -0400

    Added code to demonstrate a first code change.

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World from Ashley Ramirez!'
-
-@app.route('/hello')
-def hello_page():
-    return 'Hello World from Ashley Ramirez! This is my first HTML page'
+    return 'Hello World from Ashley Ramirez! I am adding my first code change '
 
 if __name__ == '__main__':
     app.run(port=5001)
